e01d026014e7dcc7a0dd8571f0b4c856bd5eed71
Sami Nasufovski <samellow212@gmail.com>
2023-03-24 14:32:29 +0100

    first commit

---
+++ main/main.c
@@ -0,0 +1,61 @@
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_timer.h"
+#include <rom/ets_sys.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define TRIGGER_PIN GPIO_NUM_19
+#define ECHO_PIN GPIO_NUM_35
+#define ADC_CHANNEL ADC1_CHANNEL_7
+
+#define SOUND_SPEED 0.034
+
+
+void ultrasonic_sensor_init(void)
+{
+    gpio_pad_select_gpio(TRIGGER_PIN);
+    gpio_pad_select_gpio(ECHO_PIN);
+
+    gpio_set_direction(TRIGGER_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(ECHO_PIN, GPIO_MODE_INPUT);
+
+}
+
+float ultrasonic_read_distance(void)
+{
+    uint32_t start_time, end_time, duration;
+    float distance;
+
+    // Trigger the sensor
+    gpio_set_level(TRIGGER_PIN, 1);
+    ets_delay_us(10);
+    gpio_set_level(TRIGGER_PIN, 0);
+
+    // Wait for to respond
+    while (gpio_get_level(ECHO_PIN) == 0);
+    start_time = esp_timer_get_time();
+
+    while (gpio_get_level(ECHO_PIN) == 1);
+    end_time = esp_timer_get_time();
+
+    // Calculate the distance in meters
+    duration = end_time - start_time;
+    distance = duration * SOUND_SPEED / 2;
+
+    return distance;
+}
+
+
+void app_main(void)
+{
+    // setup
+        ultrasonic_sensor_init();
+    // loop
+    while (1)
+    {
+        float distance = ultrasonic_read_distance();
+        printf("Distance: %.2fcm\n", distance * 10);
+        vTaskDelay(pdMS_TO_TICKS(1000)); // wait 500ms before reading again
+    }
+}
